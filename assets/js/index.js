dc1ee07496f83513942efecf7fbcd448ac0e5ea3
Jhao Ting Chen <jack34672@gmail.com>
2020-01-20 02:04:45 +0800

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -52,14 +52,3 @@ const setupGuides = (data) => {
     guideList.innerHTML = '<h5 class = "center-align">Login to view guides</h5>';
   }
 }
-
-// setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
-
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
-
-  var items = document.querySelectorAll('.collapsible');
-  M.Collapsible.init(items);
-
-});
