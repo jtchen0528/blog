a78ee4f8a38c419ae74cf7f66bcde7435a8f4f38
Jhao Ting Chen <jack34672@gmail.com>
2020-01-20 04:55:10 +0800

    update

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+const functions = require('firebase-functions');
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    // check if admin
+    // if (context.auth.token.admin != true){
+    //   return { error: 'only admins can add other admins.'}
+    // }
+    // get user and add admin custom claim
+    return admin.auth().getUserByEmail(data.email).then(user => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        admin: true
+      })
+    }).then(() => {
+      return {
+        message: `Success! ${data.email} has been made an admin.`
+      }
+    }).catch(err => {
+      return err;
+    });
+});
