616e46e3465becff06e9d37bf652a8a8625af120
Jhao Ting Chen <jack34672@gmail.com>
2020-01-20 02:36:37 +0800

    update

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -52,29 +52,29 @@ auth.onAuthStateChanged(user =>{
 // })
 
 
-//signup
-// const signupForm = document.querySelector("#signup-form");
-// signupForm.addEventListener('submit', (e) =>{
-//     e.preventDefault();
+// signup
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
     
-//     // get user info
-//     const email = signupForm['signup-email'].value;
-//     const password = signupForm['signup-password'].value;
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
 
-//     // sign up the user
-//     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-//         return db.collection('users').doc(cred.user.uid).set({
-//             bio: signupForm['signup-bio'].value
-//         });
-//     }).then(() => {
-//         const modal = document.querySelector('#modal-signup');
-//         M.Modal.getInstance(modal).close();
-//         signupForm.reset();
-//         signupForm.querySelector('.error').innerHTML = '';
-//     }).catch(err => {
-//         signupForm.querySelector('.error').innerHTML = err.message;
-//     });
-// });
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            user: signupForm['signup-user'].value
+        });
+    }).then(() => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+        signupForm.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        signupForm.querySelector('.error').innerHTML = err.message;
+    });
+});
 
 //logout
 const logout = document.querySelector('#logout');
