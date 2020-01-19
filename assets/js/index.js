3cb8472803e65fa56008bbb0574837699b209bfd
Jhao Ting Chen <jack34672@gmail.com>
2020-01-20 03:44:30 +0800

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,7 +24,7 @@ const setupUI = (user) => {
     loggedOutLinks.forEach(item => item.style.display = 'none');
   } else {
     // account info
-    accountDetails.innerHTML = `<h1 id="title">廢文集散地</h1><p>@jack34672</p>`;
+    accountDetails.innerHTML = ``;
     adminItems.forEach(item => item.style.display = 'none');
     // toggle UI
     loggedOutLinks.forEach(item => item.style.display = 'block');
