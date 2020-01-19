4b3587ec87c47a411ed5c89eb43bfea315e5a8d2
Jhao Ting Chen <jack34672@gmail.com>
2020-01-20 05:37:01 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 exports.addAdminRole = functions.https.onCall((data, context) => {
     // check if admin
-    if (context.auth.token.admin != null){
+    if (context.auth.token.admin != true){
       return { error: 'only admins can add other admins.'}
     }
     // get user and add admin custom claim
@@ -18,3 +20,22 @@ exports.addAdminRole = functions.https.onCall((data, context) => {
       return err;
     });
   });
+
+exports.removeAdminRole = functions.https.onCall((data, context) => {
+    // check if admin
+    if (context.auth.token.admin != true){
+      return { error: 'only admins can remove other admins.'}
+    }
+    // get user and add admin custom claim
+    return admin.auth().getUserByEmail(data.email).then(user => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        admin: false
+      })
+    }).then(() => {
+      return {
+        message: `Success! ${data.email} has been removed from admin.`
+      }
+    }).catch(err => {
+      return err;
+    });
+});
