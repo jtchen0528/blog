17e1e41eecd49d7f52c661aab8d014cf6dbc4646
Jhao Ting Chen <jack34672@gmail.com>
2020-02-01 21:08:22 +0800

    update

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,7 +1,5 @@
 
 
-// console.log('testtest')
-
 // listen for auth status changes
 auth.onAuthStateChanged(user =>{
     if(user){
@@ -23,23 +21,23 @@ auth.onAuthStateChanged(user =>{
     }
 })
 
-var user = firebase.auth().currentUser;
+// var user = firebase.auth().currentUser;
 
-if (user) {
-    user.getIdTokenResult().then(idTokenResult => {
-        user.admin = idTokenResult.claims.admin;
-        setupUI(user);
-    })
-    // get data
-    db.collection('feeds').onSnapshot(snapshot => {
-        setupFeeds(snapshot.docs);
-    }, err => {
-        console.log(err.message)
-    })
-} else {
-    setupUI();
-    setupFeeds([]);
-}
+// if (user) {
+//     user.getIdTokenResult().then(idTokenResult => {
+//         user.admin = idTokenResult.claims.admin;
+//         setupUI(user);
+//     })
+//     // get data
+//     db.collection('feeds').onSnapshot(snapshot => {
+//         setupFeeds(snapshot.docs);
+//     }, err => {
+//         console.log(err.message)
+//     })
+// } else {
+//     setupUI();
+//     setupFeeds([]);
+// }
 
 
 // create new guides
